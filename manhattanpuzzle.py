9c54bb8b74e242bc151a1733349a35549a6f23c3
Mae <jcmoninghoff@gmail.com>
2020-04-22 01:03:26 -0700

    Deprintify

--- manhattanpuzzle.py
+++ manhattanpuzzle.py
@@ -12,12 +12,6 @@ def manhattanpuzzle(start, goal, size):
     print('Path length = ', len(path))
 
 def stateSearch(unexplored, goal, path, depth, size):
-
-    print(
-    #    'unexplored = ', unexplored,
-        '\npath = ', path
-    )
-
     if unexplored == [] or depth > 20:
         return []
     
@@ -29,16 +23,7 @@ def stateSearch(unexplored, goal, path, depth, size):
 
     boards = sortBoards(curr)
 
-    print(
-        'boards = ', boards
-    )
-
     for x in range(len(boards)):
-        print(
-            'board ', x + 1,
-            ' in range ', len(boards),
-            '\n', boards
-        )
         currBoard = unexplored[boards[x][0]]
         if goal == currBoard:
             return path + [goal]
@@ -52,38 +37,7 @@ def stateSearch(unexplored, goal, path, depth, size):
 
 
 def isRepeat(currBoard, path):
-
-    if len(path) > 0 and path[-1] == currBoard:
-        print(
-                'repeat:\n'
-                'currboard = ', currBoard,
-                '\npath[-1] = ', path[-1],
-                '\n path len = ', len(path)
-        )
-        return True
-    elif len(path) > 1 and (path[-2] == currBoard or path[-1] == currBoard):
-        print(
-            'repeat:\n'
-            'currboard = ', currBoard,
-            '\npath[-2] = ', path[-2],
-            '\n path len = ', len(path)
-        )
-        return True
-    elif len(path) > 2 and (path[-2] == currBoard or path[-1] == currBoard or path[-3] == currBoard):
-        print(
-            'repeat:\n'
-            'currboard = ', currBoard,
-            '\npath[-3] = ', path[-3],
-            '\n path len = ', len(path)
-        )
-        return True
-    elif len(path) > 3 and (path[-2] == currBoard or path[-1] == currBoard or path[-3] == currBoard or path[-4] == currBoard):
-        print(
-            'repeat:\n'
-            'currboard = ', currBoard,
-            '\npath[-4] = ', path[-4],
-            '\n path len = ', len(path)
-        )
+    if len(path) > 1 and (path[-2] == currBoard or path[-1] == currBoard):
         return True
     else:
         return False
@@ -91,9 +45,6 @@ def isRepeat(currBoard, path):
 
 
 def sortBoards(arr):
-    print(
-        'Tuple Array: ', arr
-    )
     for x in range(len(arr)):
         for y in range(0, len(arr)-x-1):
             if arr[y][1] > arr[y+1][1]:
@@ -114,15 +65,16 @@ def findManhattan(board, goal, size):
     return manDist
 
 
-def findTile(num, goal, size):
+def findTile(num, board, size):
     for x in range(size):
         for y in range(size):
-            if goal[x][y] == num:
+            if board[x][y] == num:
                 return [x, y]
 
 
 def findChildren(board, size):
     newStates = []
+    emptyTile = findTile(0, board, size)
     left = generateLeftMove(board, size)
     if left != None:
         newStates += [left]
@@ -141,7 +93,7 @@ def findChildren(board, size):
     return newStates
 
 
-
+#TODO: apply empty tile knowledge, eliminate searches
 def generateLeftMove(currState, size):
     localState = deepcopy(currState)
     for x in range(size):
