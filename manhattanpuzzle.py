a267f3fe256fb37d0147b3816f03582c53df87a6
Mae <jcmoninghoff@gmail.com>
2020-04-23 01:20:30 -0700

    0 Space tracking, greatly reduced searches

--- manhattanpuzzle.py
+++ manhattanpuzzle.py
@@ -1,8 +1,11 @@
 
 from copy import deepcopy
 
-def manhattanpuzzle(start, goal, size):
-    path = stateSearch([start], goal, [], 1, size)
+def manhattanpuzzle(start, goal):
+    size = len(start)
+    emptyTile = findTile(0, start, size)
+    board = (start, emptyTile)
+    path = stateSearch(board, goal, [board[0]], 1, size)
 
     for x in path:
         for y in x:
@@ -12,23 +15,28 @@ def manhattanpuzzle(start, goal, size):
     print('Path length = ', len(path))
 
 def stateSearch(unexplored, goal, path, depth, size):
-    if unexplored == [] or depth > 20:
+    if depth > 15:
         return []
-    
+    elif goal == unexplored[0]:
+        return path + [goal]
+    children = findChildren(unexplored, size)
+    print(
+        'children = ', children
+    )
     
     curr = []
-    for x in range(len(unexplored)):
-        if not isRepeat(unexplored[x], path):            
-            curr.append((x, findManhattan(unexplored[x], goal, size)))
+    for x in range(len(children)):
+        if not isRepeat(children[x][0], path):            
+            curr.append((x, findManhattan(children[x][0], goal, size)))
 
     boards = sortBoards(curr)
 
     for x in range(len(boards)):
-        currBoard = unexplored[boards[x][0]]
-        if goal == currBoard:
+        currBoard = children[boards[x][0]]
+        if goal == currBoard[0]:
             return path + [goal]
         
-        result = stateSearch(findChildren(currBoard, size),goal, path + [currBoard], depth + 1, size)
+        result = stateSearch(currBoard, goal, path + [currBoard[0]], depth + 1, size)
 
         if result != []:
             return result
@@ -69,70 +77,82 @@ def findTile(num, board, size):
     for x in range(size):
         for y in range(size):
             if board[x][y] == num:
-                return [x, y]
+                return (x, y)
 
 
 def findChildren(board, size):
+    emptyTile = board[1]
     newStates = []
-    emptyTile = findTile(0, board, size)
-    left = generateLeftMove(board, size)
+    left = generateLeftMove(board, size, emptyTile)
     if left != None:
         newStates += [left]
 
-    right = generateRightMove(board, size)    
+    right = generateRightMove(board, size, emptyTile)    
     if right != None:
         newStates += [right]
 
-    up = generateUpMove(board, size)    
+    up = generateUpMove(board, size, emptyTile)    
     if up != None:
         newStates += [up]
 
-    down = generateDownMove(board, size)
+    down = generateDownMove(board, size, emptyTile)
     if down  != None:
         newStates += [down]
     return newStates
 
 
 #TODO: apply empty tile knowledge, eliminate searches
-def generateLeftMove(currState, size):
-    localState = deepcopy(currState)
-    for x in range(size):
-        for y in range(1, size):
-            if localState[x][y] == 0:
-                left = localState[x][y - 1]
-                localState[x][y] = left
-                localState[x][y - 1] = 0
-                return localState
+def generateLeftMove(currState, size, emptyTile):
+    localState = deepcopy(currState[0])
+    x = emptyTile[0]
+    y = emptyTile[1]
+
+    print(
+        'x = ', x,
+        '\ny = ', y,
+        '\nlocalState = ', localState
+    )
+
+    if y > 0:
+        if localState[x][y] == 0:
+            left = localState[x][y - 1]
+            localState[x][y] = left
+            localState[x][y - 1] = 0
+            return (localState, (x,y-1))
 
 
-def generateRightMove(currState, size):
-    localState = deepcopy(currState)
-    for x in range(size):
-        for y in range(size - 1):
-            if localState[x][y] == 0:
-                right = localState[x][y + 1]
-                localState[x][y] = right
-                localState[x][y + 1] = 0
-                return localState
+def generateRightMove(currState, size, emptyTile):
+    localState = deepcopy(currState[0])
+    x = emptyTile[0]
+    y = emptyTile[1]
+    if y < size - 1:
+        if localState[x][y] == 0:
+            right = localState[x][y + 1]
+            localState[x][y] = right
+            localState[x][y + 1] = 0
+            return (localState, (x,y+1))            
 
 
-def generateUpMove(currState, size):
-    localState = deepcopy(currState)
-    for x in range(1, size):
-        for y in range(size):
-            if localState[x][y] == 0:
-                up = localState[x - 1][y]
-                localState[x][y] = up
-                localState[x - 1][y] = 0
-                return localState
+def generateUpMove(currState, size, emptyTile):
+    localState = deepcopy(currState[0])
+    x = emptyTile[0]
+    y = emptyTile[1]
+    if x > 0:
+        if localState[x][y] == 0:
+            up = localState[x - 1][y]
+            localState[x][y] = up
+            localState[x - 1][y] = 0
+            return (localState, (x-1,y))
+            
 
 
-def generateDownMove(currState, size):
-    localState = deepcopy(currState)
-    for x in range(2):
-        for y in range(3):
-            if localState[x][y] == 0:
-                down = localState[x + 1][y]
-                localState[x][y] = down
-                localState[x + 1][y] = 0
-                return localState
+def generateDownMove(currState, size, emptyTile):
+    localState = deepcopy(currState[0])
+    x = emptyTile[0]
+    y = emptyTile[1]
+    if x < size - 1:
+        if localState[x][y] == 0:
+            down = localState[x + 1][y]
+            localState[x][y] = down
+            localState[x + 1][y] = 0
+            return (localState, (x+1,y))
