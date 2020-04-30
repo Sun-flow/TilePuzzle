4eefc47f64d55c15f6bb3075bdcfb59fb70c36a2
Mae <jcmoninghoff@gmail.com>
2020-04-30 00:37:32 -0700

    Solve 3x3 optimally

--- astarpuzzle.py
+++ astarpuzzle.py
@@ -3,6 +3,9 @@ from queue import PriorityQueue
 import manhattanpuzzle
 
 
+def toTup(list):
+    return (tuple(list[0]), tuple(list[1]), tuple(list[2]))
+
 
 def astarpuzzle(start, goal):
     size = len(start)
@@ -10,41 +13,43 @@ def astarpuzzle(start, goal):
     board = (start, emptyTile)
     cameFrom = stateSearch(start, goal, board, 1, size)
 
-    currBoard = goal
+    currBoard = (tuple(goal[0]), tuple(goal[1]), tuple(goal[2]))
     path = [goal]
     while cameFrom[currBoard] != None:
-        path += cameFrom[currBoard]
+        path += [cameFrom[currBoard]]
         nextBoard = cameFrom[currBoard]
-        currBoard = nextBoard
+        currBoard = toTup(nextBoard)
 
-
-    print('Path length = ', len(path))
+    flipPath = path[::-1]
+    for board in flipPath:
+        print()
+        for row in board:
+            print(row)
+    print('Path length = ', len(flipPath))
 
 def stateSearch(startBoard, goal, path, depth, size):
     open = PriorityQueue()
-
+    goalTup = (tuple(goal[0]), tuple(goal[1]), tuple(goal[2]))
     dist = manhattanpuzzle.findmanhattan(startBoard, goal, size)
     open.put((dist, 0, startBoard))
-    cameFrom = {tuple(tuple(startBoard)) : None}
-    moves = {tuple(tuple(startBoard)) : 0}
-
-    print(startBoard)
+    cameFrom = {(tuple(startBoard[0]), tuple(startBoard[1]), tuple(startBoard[2])) : None}
+    moves = {(tuple(startBoard[0]), tuple(startBoard[1]), tuple(startBoard[2])) : 0}
 
     while not open.empty():
         currBoard = open.get()
         for child in manhattanpuzzle.findChildren(currBoard[2], size):
-            print(child)
-            if tuple(child) not in cameFrom:
-                dist = currBoard[1] + manhattanpuzzle.findmanhattan(tuple(child), goal, size) + 1
-                open.put(dist, currBoard[1]+1, child)
-                cameFrom[tuple(child)] = currBoard[2]
-                moves[tuple(child)] = currBoard[1]+1
+            childTup = (tuple(child[0]), tuple(child[1]), tuple(child[2]))
+            if childTup not in cameFrom:
+                dist = currBoard[1] + manhattanpuzzle.findmanhattan(childTup, goal, size) + 1
+                open.put((dist, currBoard[1]+1, child))
+                cameFrom[childTup] = currBoard[2]
+                moves[childTup] = currBoard[1]+1
             else:
-                if currBoard[1]+1 < moves[child]:
-                    moves[child[0]] = currBoard[1]+1
-                    cameFrom[child[0]] = currBoard[2]
+                if currBoard[1]+1 < moves[childTup]:
+                    moves[childTup] = currBoard[1]+1
+                    cameFrom[childTup] = currBoard[2]
 
-            if goal == child[0]:
+            if goalTup == childTup:
                 return cameFrom
             
 
